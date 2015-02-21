4aac11c272576f26e76af4a03ef29f8d4b1d02d5
c-lombardi <clombardi@problemsolutions.net>
2015-02-21 18:58:40 -0500

    added file

---
+++ StartupEducationWeekend/StartupEducationWeekend/Views/Account/WallController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StartupEducationWeekend.Views.Account
+{
+    public class WallController : Controller
+    {
+        //
+        // GET: /Wall/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
